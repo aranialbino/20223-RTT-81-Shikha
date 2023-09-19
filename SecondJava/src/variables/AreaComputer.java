ee0655c63b3cd4465d233461b2a3b5dfbccd3dce
bswr <aranialbino@gmail.com>
2023-09-19 15:16:25 -0500

    This is my todays prroject.

--- SecondJava/src/variables/AreaComputer.java
+++ SecondJava/src/variables/AreaComputer.java
@@ -1,20 +1,22 @@
 package variables;
 
 public class AreaComputer {
-public static void main (String[]args) {
-    double radius;
-    double area;
-    
-    // Assign a radius
-    radius = 20;
-    
-    // Compute area
-    area = radius * radius * 3.14159;
-    
-    // Display results
-    System.out.println("The area for the circle of radius " +
-      radius + " is " + area);
+	public static void main(String[] args) {
+		double radius;
+		double area;
 
-	
-}
+		// Assign a radius
+		radius = 20;
+
+		// Compute area
+		area = radius * radius * 3.14159;
+
+		// Display results
+		System.out.println("The area for the circle of radius " + radius + " is " + area);
+		int x = 10;
+		if ((x > 3) && (x < 20))
+			;
+		System.out.println("x is between 3 and 20");
+
+	}
 }
