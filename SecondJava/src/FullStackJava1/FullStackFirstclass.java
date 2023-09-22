14d4fda3280ddae543b320c72383615e5285bca8
bswr <aranialbino@gmail.com>
2023-09-22 11:13:59 -0500

    work of 9/22/2023

---
+++ SecondJava/src/FullStackJava1/FullStackFirstclass.java
@@ -0,0 +1,18 @@
+package FullStackJava1;
+
+public class FullStackFirstclass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		// this is my first code of class.
+
+		int x = 5;
+		System.out.println("x = " + x);
+		System.out.println("x value is ");
+		System.out.println(x);
+
+	}
+}
+/*
+ * this code is looking silly. I dont think so.Its my first try}
+ */
---
+++ SecondJava/src/FullStackJava1/IfElseLoop.java
@@ -0,0 +1,20 @@
+package FullStackJava1;
+public class IfElseLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int score = 70;
+		if (score > 90) {
+			System.out.println("your score\t a");
+		} else if (score > 80) {
+			System.out.println("your score\t \'b\'");
+		} else if (score > 70) {
+			System.out.println("your score\t \"c\"");
+		} else if (score > 60) {
+			System.out.println("your score\t \"d\"");
+
+		} else {
+			System.out.println("your score\t \'f\'");
+
+		}}}
+	
